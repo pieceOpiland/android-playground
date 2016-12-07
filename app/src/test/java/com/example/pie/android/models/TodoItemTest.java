c536c926e4e3018fdce93f08e261d4354d48ed47
Nolan Piland <8548667+pieceOpiland@users.noreply.github.com>
2016-12-07 18:09:22 -0500

    Adding a simple unit test.

---
+++ app/src/test/java/com/example/pie/android/models/TodoItemTest.java
@@ -0,0 +1,31 @@
+package com.example.pie.android.models;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class TodoItemTest {
+
+    private TodoItem testObj;
+
+    @Before
+    public void setUp() {
+        testObj = new TodoItem();
+    }
+
+    @Test
+    public void testGettersAndSetters() {
+        int id = 23;
+        String task = "Do android testing";
+        boolean isDone = false;
+
+        testObj.setId(id);
+        testObj.setTask(task);
+        testObj.setDone(isDone);
+
+        Assert.assertEquals(isDone, testObj.isDone());
+        Assert.assertEquals(id, testObj.getId());
+        Assert.assertEquals(task, testObj.getTask());
+    }
+}
