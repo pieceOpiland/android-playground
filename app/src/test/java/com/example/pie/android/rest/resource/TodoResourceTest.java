31ca57455770efc2344dc40cb790aa14788cd868
Nolan Piland <8548667+pieceOpiland@users.noreply.github.com>
2017-04-08 09:01:32 -0400

    Correcting rxjava testing. Properly using test classes.

--- app/src/test/java/com/example/pie/android/rest/resource/TodoResourceTest.java
+++ app/src/test/java/com/example/pie/android/rest/resource/TodoResourceTest.java
@@ -3,29 +3,41 @@ package com.example.pie.android.rest.resource;
 import com.example.pie.android.model.TodoItem;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoRule;
 
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Single;
 import io.reactivex.annotations.NonNull;
 import io.reactivex.functions.Consumer;
+import io.reactivex.functions.Predicate;
+import io.reactivex.observers.TestObserver;
 import io.reactivex.plugins.RxJavaPlugins;
 import io.reactivex.schedulers.Schedulers;
+import io.reactivex.schedulers.TestScheduler;
 
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
 public class TodoResourceTest {
 
+    @Rule public final MockitoRule rule = MockitoJUnit.rule();
+    @Mock private TodoResource.TodoApi mockApi;
+
     private TodoResource testObj;
-    private TodoResource.TodoApi mockApi;
+    private TestScheduler scheduler;
 
     @Before
     public void setUp() {
-        mockApi = mock(TodoResource.TodoApi.class);
-        testObj = new TodoResource(mockApi, Schedulers.trampoline(), Schedulers.trampoline());
+        scheduler = new TestScheduler();
+        testObj = new TodoResource(mockApi, scheduler, scheduler);
     }
 
     @Test
@@ -33,7 +45,21 @@ public class TodoResourceTest {
         when(mockApi.getItems())
                 .thenReturn(Single.just(Collections.<TodoItem>emptyList()));
 
-        testObj.getItems();
+        TestObserver<List<TodoItem>> testObserver = testObj.getItems().test();
+
+        testObserver.assertNotComplete();
+
+        scheduler.triggerActions();
+
+        testObserver.assertComplete()
+                .assertNoErrors()
+                .assertValue(new Predicate<List<TodoItem>>() {
+            @Override
+            public boolean test(@NonNull List<TodoItem> todoItems) throws Exception {
+                return todoItems.size() == 0;
+            }
+        });
+
         inOrder(mockApi).verify(mockApi, times(1)).getItems();
     }
 }
